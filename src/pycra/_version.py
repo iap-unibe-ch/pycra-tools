8b5d0e30ebb1a7c05bd3da739bc798bfa6feed7f
ps23c382 <philipp.schmid@unibe.ch>
2023-12-12 16:19:12 +0100

    modify dependencies

--- src/pycra/_version.py
+++ src/pycra/_version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.2.dev31+g462689c'
-__version_tuple__ = version_tuple = (0, 1, 2, 'dev31', 'g462689c')
+__version__ = version = '0.1.2.dev36+g24a6419.d20231212'
+__version_tuple__ = version_tuple = (0, 1, 2, 'dev36', 'g24a6419.d20231212')
